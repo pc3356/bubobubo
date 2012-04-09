2192f5eae597967661c562b77131e6f3e486159a
Philip Coates <phili.coates@epeirogenic.com>
2012-04-09 16:09:51 +0100

    Cleared out a load of unnecessary stuff to do with Accept headers, and fixed the response by adding the original response's type

--- src/main/java/com/bubobubo/server/Proxy.java
+++ src/main/java/com/bubobubo/server/Proxy.java
@@ -2,21 +2,21 @@ package com.bubobubo.server;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.ViewResource;
 import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.spi.component.ProviderServices;
-import com.sun.jersey.core.spi.factory.MessageBodyFactory;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.ws.rs.*;
-import javax.ws.rs.core.*;
-import java.net.URI;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
 
 @Component
 @Path("/connection")
 public class Proxy {
+
+    private final static Logger LOGGER = Logger.getLogger(Proxy.class);
 
     @GET
     public Response get(@Context Request request) throws Exception {
@@ -24,19 +24,9 @@ public class Proxy {
         Client c = Client.create();
         String uri = "http://static.bbci.co.uk/h4discoveryzone/ic/newsimg/media/images/229/129/59496000/jpg/_59496825_59496662.jpg";
         WebResource r = c.resource(uri);
-        MediaType[] mediaTypes = {
-                MediaType.APPLICATION_OCTET_STREAM_TYPE,
-                MediaType.APPLICATION_JSON_TYPE,
-                MediaType.APPLICATION_XHTML_XML_TYPE,
-                MediaType.APPLICATION_XML_TYPE
-        };
-        r.accept(mediaTypes);
         ClientResponse response = r.get(ClientResponse.class);
-
-        String entity = response.getEntity(String.class);
-
-        return Response.status(response.getClientResponseStatus()).entity(entity).build();
-
+        byte[] entity = response.getEntity(byte[].class);
+        return Response.status(response.getClientResponseStatus()).entity(entity).type(response.getType()).build();
 
     }
 
