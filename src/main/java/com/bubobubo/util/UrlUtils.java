e77e931cd0d845468e4428d77acf6380d46de5b0
Philip Coates <phili.coates@epeirogenic.com>
2012-04-22 23:26:08 +0100

    added Mockito to POM

---
+++ src/main/java/com/bubobubo/util/UrlUtils.java
@@ -0,0 +1,10 @@
+package com.bubobubo.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pjc
+ * Date: 21/04/2012
+ * Time: 21:10
+ */
+public class UrlUtils {
+}
---
+++ src/test/java/com/bubobubo/util/UrlUtilsTest.java
@@ -0,0 +1,12 @@
+package com.bubobubo.util;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pjc
+ * Date: 21/04/2012
+ * Time: 21:28
+ */
+public class UrlUtilsTest extends TestCase {
+}
